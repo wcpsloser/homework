293f6b6ac198fe481e697705c280ec8006cc486d
Mon Chalida <monchalida@wcpslosers-MacBook-Pro.local>
2020-04-06 01:32:27 +0700

    Tshirt2

--- Homework/main.swift
+++ Homework/main.swift
@@ -13,29 +13,36 @@ class Tshirt
     var size: String
     var colour: String
     var price: Float
+    var image: String? = "image"
     
     
     init(size: String,
          colour: String,
-         price: Float) {
+         price: Float,
+         image: String? = "image")
+    {
         self.size = size
         self.colour = colour
         self.price = price
+        self.image = image
     }
     var detail: String
     {
-        return "\(size) \(colour) \(price)"
+        return "\(size) \(colour) \(price) \(String(describing: image))"
     }
 }
 let sizeS = Tshirt(size: "S",
                    colour: "Black",
-                   price: 625)
+                   price: 625,
+                   image: "Photo")
 
 let sizeM = Tshirt(size: "M",
                    colour: "Black",
-                   price: 750)
+                   price: 750,
+                   image: "Photo")
 
 let sizeL = Tshirt(size: "L",
                    colour: "Red",
-                   price: 750)
+                   price: 750,
+                   image: "Photo")
 print("Tshirt : \(sizeS.size) \(sizeS.colour) \(sizeS.price)")
